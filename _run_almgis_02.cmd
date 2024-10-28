8ea323e512b48610e1fd25e6cfee899c5fd70426
gidhoog <franz.fisch@mailbox.org>
2024-10-28 13:51:45 +0100

    stelle _run_amgis_02.cmd (wird verwendet, wenn die app ohne pycharm einfach so aufgerufen werden soll)

--- _run_almgis_02.cmd
+++ _run_almgis_02.cmd
@@ -3,30 +3,29 @@ rem @ECHO off
 set OSGEO4W_ROOT=C:\work\_anwendungen\OSGeo4W
 
 call "%OSGEO4W_ROOT%\bin\o4w_env.bat"
-call "%OSGEO4W_ROOT%\bin\qt5_env.bat"
-call "%OSGEO4W_ROOT%\bin\py3_env.bat"
-call "%OSGEO4W_ROOT%\apps\grass\grass78\etc\env.bat"
+rem call "%OSGEO4W_ROOT%\bin\qt5_env.bat"
+rem call "%OSGEO4W_ROOT%\bin\py3_env.bat"
+rem call "%OSGEO4W_ROOT%\apps\grass\grass78\etc\env.bat"
 
-path %OSGEO4W_ROOT%\apps\qgis\bin;%PATH%
-set QGIS_PREFIX_PATH=%OSGEO4W_ROOT%\apps\qgis
+set PATH=%OSGEO4W_ROOT%\apps\qgis-ltr\bin;C:\work\Projekte\AlmGIS\almgis\venv_02\Scripts;%OSGEO4W_ROOT%\apps\Qt5\bin;%PATH%
+
+set O4W_QT_BINARIES=%OSGEO4W_ROOT%\apps\Qt5\bin
+set O4W_QT_DOC=%OSGEO4W_ROOT%\apps\Qt5\doc
+set O4W_QT_HEADERS=%OSGEO4W_ROOT%\apps\Qt5\include
+set O4W_QT_LIBRARIES=%OSGEO4W_ROOT%\apps\Qt5\lib
+set O4W_QT_PLUGINS=%OSGEO4W_ROOT%\apps\Qt5\plugins
+set O4W_QT_PREFIX=%OSGEO4W_ROOT%\apps\Qt5
+set O4W_QT_TRANSLATIONS=%OSGEO4W_ROOT%\apps\Qt5\translations
+set QT_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\qgis-ltr\qtplugins;%OSGEO4W_ROOT%\apps\Qt5\plugins
+
+rem set QGIS_PREFIX_PATH=%OSGEO4W_ROOT:\=/%/apps/qgis-ltr
 
 set GDAL_FILENAME_IS_UTF8=YES
 
 set VSI_CACHE=TRUE
 set VSI_CACHE_SIZE=1000000
-set QT_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\qgis-ltr\qtplugins;%OSGEO4W_ROOT%\apps\qt5\plugins
 
-set PYTHONPATH=%OSGEO4W_ROOT%\apps\qgis-ltr\python
-set PYTHONHOME=%OSGEO4W_ROOT%\apps\Python312
-set PYTHONPATH=%OSGEO4W_ROOT%\apps\Python312\lib\site-packages;%PYTHONPATH%
-
-set QT_QPA_PLATFORM_PLUGIN_PATH=%OSGEO4W_ROOT%\apps\Qt5\plugins\platforms
-set QGIS_PREFIX_PATH=%OSGEO4W_ROOT%\apps\qgis-ltr
-
-set PYTHONPATH=%OSGEO4W_ROOT%\apps\qgis-ltr\python\plugins;%PYTHONPATH%
-set PYTHONPATH=C:\work\Projekte\AlmGIS\almgis\venv\Lib\site-packages;%PYTHONPATH%
-
-set PATH=%OSGEO4W_ROOT%\apps\qgis-ltr\bin;%OSGEO4W_ROOT%\apps\qgis-ltr\python\plugins;%OSGEO4W_ROOT%\apps\qgis-ltr\python\plugins\processing;%OSGEO4W_ROOT%\apps\qgis-ltr\python\qgis;%OSGEO4W_ROOT%\bin;%PATH%
+set PYTHONPATH=%OSGEO4W_ROOT%\bin;%OSGEO4W_ROOT%\apps\qgis-ltr\python;%OSGEO4W_ROOT%\apps\qgis-ltr\python\plugins;%OSGEO4W_ROOT%\apps\Python312\lib\site-packages;%PYTHONPATH%
 
 @call C:/work/Projekte/AlmGIS/almgis/venv/Skripts/activate.bat
 python almgis.py
