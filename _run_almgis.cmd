d985af601067c808f7bc533a5156ee8f7f623470
gidhoog <franz.fisch@mailbox.org>
2025-02-12 09:38:48 +0100

    edit _run_almgis.cmd

--- _run_almgis.cmd
+++ _run_almgis.cmd
@@ -7,7 +7,7 @@ rem call "%OSGEO4W_ROOT%\bin\qt5_env.bat"
 rem call "%OSGEO4W_ROOT%\bin\py3_env.bat"
 rem call "%OSGEO4W_ROOT%\apps\grass\grass78\etc\env.bat"
 
-set PATH=%OSGEO4W_ROOT%\apps\qgis-ltr\bin;C:\work\Projekte\AlmGIS\almgis\venv_02\Scripts;%OSGEO4W_ROOT%\apps\Qt5\bin;%PATH%
+set PATH=%OSGEO4W_ROOT%\apps\qgis-ltr\bin;C:\work\Projekte\AlmGIS\.venv\Scripts;%OSGEO4W_ROOT%\apps\Qt5\bin;%PATH%
 
 set O4W_QT_BINARIES=%OSGEO4W_ROOT%\apps\Qt5\bin
 set O4W_QT_DOC=%OSGEO4W_ROOT%\apps\Qt5\doc
@@ -27,6 +27,6 @@ set VSI_CACHE_SIZE=1000000
 
 set PYTHONPATH=%OSGEO4W_ROOT%\bin;%OSGEO4W_ROOT%\apps\qgis-ltr\python;%OSGEO4W_ROOT%\apps\qgis-ltr\python\plugins;%OSGEO4W_ROOT%\apps\Python312\lib\site-packages;%PYTHONPATH%
 
-@call C:/work/Projekte/AlmGIS/almgis/venv/Skripts/activate.bat
+@call C:/work/Projekte/AlmGIS/.venv/Skripts/activate.bat
 python almgis.py
 pause
--- run_almgis.cmd
+++
@@ -1,3 +0,0 @@
-@call C:/work/Projekte/AlmGIS/almgis/venv/Skripts/activate.bat
-python almgis.py
-pause
