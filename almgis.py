77572a747ec702d924f8d212aa25c747a7bc4867
gidhoog <franz.fisch@mailbox.org>
2025-05-15 08:49:24 +0200

    improve splash

--- almgis.py
+++ almgis.py
@@ -1,10 +1,11 @@
 import platform
 import sys
 from time import sleep, time
+
+from qga.splash import QgaSplash
 from qgis.core import QgsApplication
-from PyQt5.QtCore import Qt, QRect
-from PyQt5.QtGui import QPixmap, QFont, QColor
-from PyQt5.QtWidgets import QSplashScreen
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
 
 from almgis import settings_user, settings_app
 """import after the settings"""
@@ -27,7 +28,7 @@ def run():
     """use a splashscreen"""
     start = time()
     pixmap = QPixmap("splash.png")
-    splash = CustomSplashScreen(pixmap)
+    splash = QgaSplash(pixmap)
     splash.showMessage("Die Anwendung wird geladen, "
                        "bitte warten ...",
                        Qt.black)
@@ -43,8 +44,6 @@ def run():
     Logger.info("+ + + + AlmGis gestartet !!! + + + +")
     Logger.info(f'setting_app file_name: {settings_app.fileName()}')
     Logger.info(f'setting_user file_name: {settings_user.fileName()}')
-
-
 
     if platform.system() == 'Linux':
         app.setPrefixPath("/var/lib/flatpak/app/org.qgis.qgis",
@@ -67,36 +66,6 @@ def run():
     splash.finish(main_window)
 
     sys.exit(app.exec_())
-
-class CustomSplashScreen(QSplashScreen):
-    def __init__(self, pixmap):
-        super().__init__(pixmap)
-        self.message = ""
-        self.color = Qt.black
-        self.font = QFont("Arial", 12, QFont.Bold)
-
-    def showMessage(self, message, color=Qt.black, font=None):
-        self.message = message
-        self.color = color
-        if font:
-            self.font = font
-        self.update()
-
-    def drawContents(self, painter):
-        painter.save()
-        painter.setPen(QColor(self.color))
-        painter.setFont(self.font)
-        # Define a rectangle at the bottom of the splash screen
-        margin = 20
-        rect = QRect(
-            margin,
-            self.height() - margin - 30,  # 30 is an arbitrary height for the text area
-            self.width() - 2 * margin,
-            30
-        )
-        painter.drawText(rect, Qt.AlignCenter | Qt.AlignVCenter, self.message)
-        painter.restore()
-
 
 
 if __name__ == '__main__':
