cdadccbd1a45659ce5ba6ad350147d977508465d
gidhoog <gz7_m7@mailbox.org>
2025-06-28 09:33:04 +0200

    improve menu in main_window_gui.py

--- almgis/gui/main_window_gui.py
+++ almgis/gui/main_window_gui.py
@@ -1,4 +1,6 @@
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QAction
 from qga.gui.main_window_gui import QgaMainWindowGui
 
 
@@ -9,3 +11,16 @@ class AlmMainWindowGui(QgaMainWindowGui):
 
         # self.useMenuBar = False
 
+    def setupActions(self):
+        super().setupActions()
+
+        self.actionAlleAkte = QAction()
+        self.actionAlleAkte.setText('alle Akte')
+        self.actionAlleAkte.setIcon(
+            QIcon(':/svg/icons/mActionFileNew.svg'))
+
+    def setupMenuBar(self):
+        super().setupMenuBar()
+
+        self.menuAkte = self.menuBar().addMenu('Akte')
+        self.menuAkte.addAction(self.actionAlleAkte)
