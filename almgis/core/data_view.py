0e2f1a5d15b80204fe355eb99dfb69fb648d2fb7
gidhoog <franz.fisch@mailbox.org>
2025-09-29 15:21:18 +0200

    set the selection color in data-view's

--- almgis/core/data_view.py
+++ almgis/core/data_view.py
@@ -10,11 +10,12 @@ class AlmDataView(QgaDataView):
 
     # _entity_dialog_class = AlmEntityDialog
 
+    settings_colors = settings_colors
+
     def __init__(self, gis_mode=False):
         super(__class__, self).__init__(gis_mode)
 
         self.session = ProjectSessionCls()
-        self.settings_colors = settings_colors
 
     def __call__(self, *args, **kwargs):
         pass
